d41f2d37d62f7388f0756e16e10bfa1bc91a6233
Bastian Silder <bsilder@uni-muenster.de>
2026-02-19 20:38:42 +0100

    correct zoom for recoding

--- gymnasium/envs/box2d/car_racing.py
+++ gymnasium/envs/box2d/car_racing.py
@@ -47,8 +47,8 @@ SCALE = 6.0  # Track scale
 TRACK_RAD = 900 / SCALE  # Track is heavily morphed circle with this radius
 PLAYFIELD = 2000 / SCALE  # Game over boundary
 FPS = 30  # Frames per second
-ZOOM = 2.5  # Camera zoom
-# ZOOM = 5.7  # Camera zoom
+# ZOOM = 2.5  # Camera zoom
+ZOOM = 5.7  # Camera zoom
 ZOOM_FOLLOW = True  # Set to False for fixed view (don't use zoom)
 
 
